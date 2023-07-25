5bbd1bf71306d8070ddd6cc50627a273dadcbd2c
uttej <98679920+uttejterlapu@users.noreply.github.com>
2023-07-25 11:34:11 +0530

    Update vaccum.py
    used agentfunction

--- vaccum.py
+++ vaccum.py
@@ -5,6 +5,39 @@ class room:
     def suck(self):
         self.dirty = False
 
+def agentFunction(vaccumpos):
+    if vaccumpos=="A" and A.dirty and B.dirty:
+        A.suck()
+        print("cleaned A")
+        print("moving to B")
+        vaccumpos == "B"
+        B.suck()
+        print("cleaned B")
+        sys.exit()
+
+    elif vaccumpos=="A" and A.dirty and not B.dirty:  
+        A.suck()
+        print("cleaned A")
+        sys.exit()
+    
+    elif vaccumpos=="B" and A.dirty and B.dirty:
+        B.suck()
+        print("cleaned B")
+        print("moving to A")
+        vaccumpos == "A"
+        A.suck()
+        print("cleaned A")
+        sys.exit()
+    
+    elif vaccumpos=="B" and not A.dirty and B.dirty:
+        B.suck()
+        print("cleaned B")
+        sys.exit()
+    
+    elif not A.dirty and not B.dirty:
+        print("Both rooms are already clean Total cost = 0")
+        sys.exit()
+
 A = room()
 B = room()
 
@@ -13,34 +46,4 @@ B.dirty = input("Is room B dirty? Enter True/False: ").lower()=="true"
 vaccumpos= input("Is vaccum cleaner present in room A or B :").upper()
 print("vaccum cleaner present in room ",vaccumpos)
 
-if vaccumpos=="A" and A.dirty and B.dirty:
-    A.suck()
-    print("cleaned A moving to B")
-    vaccumpos == "B"
-    B.suck()
-    print("cleaned B")
-    sys.exit()
-
-elif vaccumpos=="A" and A.dirty and not B.dirty:  
-    A.suck()
-    print("cleaned A")
-    sys.exit()
-    
-elif vaccumpos=="B" and A.dirty and B.dirty:
-    B.suck()
-    print("cleaned B moving to A")
-    vaccumpos == "A"
-    A.suck()
-    print("cleaned A")
-    sys.exit()
-    
-elif vaccumpos=="B" and not A.dirty and B.dirty:
-    B.suck()
-    print("cleaned B")
-    sys.exit()
-    
-elif not A.dirty and not B.dirty:
-    print("Both rooms are already clean Total cost = 0")
-    sys.exit()
-
-print("all rooms are cleaned")
+agentFunction(vaccumpos)
